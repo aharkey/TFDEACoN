dac5554a557cc937a47ce16ad5ca3786df5c9687
Andria Harkey <aharkey@wakehealth.edu>
2019-12-11 13:22:30 -0500

    Cleaning up code: -Changing variable names to make more consistent -Adding commentary -Removing some redundancy

--- EnrichSearch/app.R
+++ EnrichSearch/app.R
@@ -4,93 +4,107 @@ ui <- fluidPage(
   titlePanel("TF DEACoN"),
   
   sidebarLayout(
-    sidebarPanel(h3("Input Genes Here"),
-                 textAreaInput("genes", label = "Gene IDs (AT IDs)",
+    sidebarPanel(h3("Input Genes"),
+                 textAreaInput("query", label = "Gene IDs (AT IDs)",
                                width = "100%", rows = 5, resize = "vertical",
                                placeholder = "Separators (commas, etc.) are not important. All valid AT IDs will be recognized, and other text will be ignored."),
                  actionButton("submit", "Submit")
     ),
     mainPanel(h3("Results"),
-              DT::dataTableOutput("results"),
+              DT::dataTableOutput("resultsdisplay"),
               uiOutput("downloadrender"),
-              htmlOutput("summary")
+              htmlOutput("confirmquery")
               )
     )
   )
 
 
 server <- function(input, output) {
+# Read input and find all valid gene names
+  # Find location of all valid gene names
+  locs <- eventReactive(input$submit, {
+    gregexpr("AT[1-5]G[0-9][0-9][0-9][0-9]0", input$query, ignore.case = TRUE)
+  })
+  
+  # Create a list of all valid gene names using loc
+  # Include validation that there is at least 1 valid AT ID
+  querylist <- eventReactive(input$submit, {
+    validate(
+      need(as.vector(locs()[[1]]) != -1, "Please enter a valid AT ID")
+      )
+    toupper(regmatches(input$query, locs())[[1]])
+  })
+  
+  # Output the query list
+  # This allows the user to double-check that all their gene IDs were recognized
+  output$confirmquery <- renderUI({
+    str1 <- paste(sort(querylist()), sep = " ", collapse = " ")
+    str2 <- paste("Number of genes:", querylen())
+    
+    HTML(paste(paste("<h3>Input Genes</h3>", str1, sep = ""), str2, sep = "<br/>"))
+  })
+# Read data needed:
+  # alltarg = all TF to target relationships, pairwise
   alltarg <- read.csv("input/alltarg.txt",
                       header = TRUE,
                       row.names = 1)
   
+  # targcounts = genome target counts for all TFs
+  #   becomes basis of results dataframe
   targcounts <- read.delim("input/targcounts.txt",
                          header = TRUE,
                          row.names = 1)
   
-  colnames(targcounts) <- c("TF ID", "Family", "Gene name(s)", "All targets", "Background ratio")
+# Perform calculations
+  # How many genes are in the query?
+  querylen <- reactive({length(querylist())})
   
-  locs <- eventReactive(input$submit, {
-    gregexpr("AT[1-5]G[0-9][0-9][0-9][0-9]0", input$genes, ignore.case = TRUE)
-  })
+  # Create a smaller version of alltarg which only contains the relevant entries (with query targets)
+  querytarg <- reactive({alltarg[which(alltarg$target %in% querylist()),]})
+  rm(alltarg)
   
-  genelist <- eventReactive(input$submit, {
-    toupper(regmatches(input$genes, locs())[[1]])
-  })
-  
-  targnum <- reactive({length(genelist())})
-  
-  subtarg <- reactive({alltarg[which(alltarg$target %in% genelist()),]})
-  
-  output$summary <- renderUI({
-    validate(
-      need(as.vector(locs()[[1]]) != -1, "Please enter a valid AT number")
-    )
-    str1 <- paste(sort(genelist()), sep = " ", collapse = " ")
-    str2 <- paste("Number of genes:", targnum())
-    
-    HTML(paste(paste("<h3>Input Genes</h3>", str1, sep = ""), str2, sep = "<br/>"))
-  })
-  
+# Create master "results" data frame with calculated ratio, logFC, and p-value  
   results <- reactive({
-    targcounts$"Query targets" <- NA
+    querytarg <- querytarg()
     
-    subtarg <- subtarg()
-    
-    for (i in 1:nrow(targcounts))
-    {
-      targcounts$"Query targets"[i] <- nrow(subtarg[which(subtarg$TF == targcounts$"TF ID"[i]),])
-    }
-    
-    targcounts$"Query ratio" <- targcounts$"Query targets" / targnum()
-    
-    targcounts$logFC <- log2(targcounts$"Query ratio" / targcounts$"Background ratio")
-    
-    targcounts$"Adj P value" <- NA
-    
-    for(i in 1:nrow(targcounts))
-    {
-      targcounts$"Adj P value"[i] <- as.numeric(binom.test(targcounts$"Query targets"[i], targnum(), targcounts$"Background ratio"[i],
-                                                           alternative = "two.sided")$p.value)
-    }
-    
+    # Count the number of query targets each TF has
     results <- targcounts
+    results$query.count <- NA
+    for (i in 1:nrow(results))
+    {results$query.count[i] <- nrow(querytarg[which(querytarg$TF == results$tf.id[i]),])}
     
-    results <- results[order(results$"Adj P value", results$logFC),]
+    # Calculate the query ratio and logFC
+    results$query.ratio <- results$query.count / querylen()
+    
+    results$logFC <- log2(results$query.ratio / results$genome.ratio)
+    
+    # Calculate the adjusted p-value
+    results$adj.pval <- NA
+    
+    for(i in 1:nrow(results))
+    {
+      results$adj.pval[i] <- as.numeric(binom.test(results$query.count[i], querylen(), results$genome.ratio[i],
+                                                           alternative = "two.sided")$p.value)
+      }
+    
+    # Arrange results from smallest to largest p-value
+    results <- results[order(results$adj.pval, results$logFC),]
   })
   
-  resultsdisplay <- reactive({
+# Create version of results for displaying on screen
+  output$resultsdisplay <- DT::renderDataTable({
     resultsdisplay <- results()
     
+    # Round numeric results for display
     resultsdisplay[,c(5,7:9)] <- round(resultsdisplay[,c(5,7:9)], digits = 2)
     
-    resultsdisplay <- resultsdisplay
+    # Add better-looking column names
+    DT::datatable(resultsdisplay, rownames = FALSE,
+                  colnames = c("TF ID", "TF family", "Gene name(s)", "Genome target count", "Genome ratio", "Query target count", "Query ratio", "logFC", "Adj p-value")
+                  )
   })
-  
-  output$results <- DT::renderDataTable({  
-    DT::datatable(resultsdisplay(), rownames = FALSE)
-  })
-  
+
+# Create the download file 
   output$downloadinfo <- downloadHandler(
     filename = function() {
       paste("TFDEACoN_Output_", Sys.time(), ".csv", sep = "")
@@ -100,13 +114,11 @@ server <- function(input, output) {
       write.csv(results(), file, row.names = FALSE)
     })
   
+# Render the download button  
   output$downloadrender <- renderUI({
     req(input$submit)
     downloadButton("downloadinfo")
   })
 }
-
-
-
 
 shinyApp(ui = ui, server = server)
